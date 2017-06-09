edf752dc690c4970f9622334a2f9047af33f6de1
chenlijun <419166044@qq.com>
2017-06-10 03:09:41 +0800

    测试

---
+++ carBoss-web/src/main/java/com/pp/web/Demo.java
@@ -0,0 +1,13 @@
+package com.pp.web;
+
+/**
+ * Created by chenlijun on 2017/6/10.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Teacher teacher=new Teacher();
+        teacher.setUserName("123");
+        teacher.setAge("23");
+        System.out.println(teacher);
+    }
+}
---
+++ carBoss-web/src/main/java/com/pp/web/Teacher.java
@@ -0,0 +1,13 @@
+package com.pp.web;
+
+import lombok.Data;
+
+/**
+ * Created by chenlijun on 2017/6/10.
+ */
+@Data
+public class Teacher {
+
+    private String userName;
+    private String age;
+}
